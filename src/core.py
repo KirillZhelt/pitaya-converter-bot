ea81afffe8d2b6047d52983d30b07a2dfbad5113
Kirill Zheltovski <kirilzh2000@gmail.com>
2018-12-13 21:10:03 +0300

    Add tiny functional (now bot prints binary representation for positive integers)

--- src/core.py
+++ src/core.py
@@ -10,20 +10,27 @@ def start(bot, update):
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
+def numbers(bot, update):
+    # TODO: should decide what to do with negative numbers
+    bot.send_message(chat_id=update.message.chat_id, text=str(bin(int(update.message.text))))
 
-if os.path.isfile("..\\token"):
-    with open("..\\token") as token_file:
+
+if os.path.isfile("token"):
+    with open("token") as token_file:
         private_token = token_file.readline()
 
+# TODO: handle invalid token
 updater = Updater(token=private_token)
 dispatcher = updater.dispatcher
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 start_handler = CommandHandler("start", start)
-echo_handler = MessageHandler(Filter.text, echo)
+numbers_handler = MessageHandler(Filters.regex("^(-?[1-9]+\d*)$|^0$"), numbers)
+echo_handler = MessageHandler(Filters.text, echo)
+
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(numbers_handler)
 dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
 updater.idle()
-
