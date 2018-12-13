0e67965570ab6368588f9ced756e0e86ab840531
Kirill Zheltovski <kirilzh2000@gmail.com>
2018-12-13 18:49:48 +0300

    Add response for text messages

--- src/core.py
+++ src/core.py
@@ -1,11 +1,14 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import logging
 import os
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Дима фуРс петуч")
+    bot.send_message(chat_id=update.message.chat_id, text="Hi, it is Pitaya Converter.")
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
 
 if os.path.isfile("..\\token"):
@@ -17,7 +20,9 @@ dispatcher = updater.dispatcher
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 start_handler = CommandHandler("start", start)
+echo_handler = MessageHandler(Filter.text, echo)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
 updater.idle()
