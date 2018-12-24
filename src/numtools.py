de7e293f8c1085e062290ff6162643b189980989
Kirill Zheltovski <kirilzh2000@gmail.com>
2018-12-24 16:00:33 +0300

    Implement string parsing in Number __init__

--- src/numtools.py
+++ src/numtools.py
@@ -8,8 +8,17 @@ class NumeralSystem(enum.Enum):
 
 class Number:
     def __init__(self, s: str):
-        self.number = int(s) # TODO: impement parsing
-        self.numeral_system = NumeralSystem.Decimal
+        s = s.lstrip().lower()
+
+        if s.startswith("0x") != -1:
+            self.number = int(s, 16)
+            self.numeral_system = NumeralSystem.Hexadecimal
+        elif s.startswith("0b") != -1:
+            self.number = int(s, 2)
+            self.numeral_system = NumeralSystem.Binary
+        else:
+            self.number = int(s, 10)
+            self.numeral_system = NumeralSystem.Decimal
 
     def to_decimal(self) -> str:
         return str(self.number)
@@ -21,6 +30,9 @@ class Number:
     def to_hex(self) -> str:
         # TODO: implement 
         return "0xaffa6"
+
+    def __str__(self):
+        return str(self.number)
 
     convert_functions = {
         NumeralSystem.Decimal: to_decimal,
